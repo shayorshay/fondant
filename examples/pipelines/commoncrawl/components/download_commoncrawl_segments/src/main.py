32837e5bc688d21ebd4d4cea937600868fdc204e
Sharon Grundmann <sharon.grundmann@ml6.eu>
2023-07-17 16:59:55 +0200

    Use apply for processing WARC files

--- examples/pipelines/commoncrawl/components/download_commoncrawl_segments/src/main.py
+++ examples/pipelines/commoncrawl/components/download_commoncrawl_segments/src/main.py
@@ -1,3 +1,5 @@
+import os
+import time
 import logging
 from typing import List
 
@@ -6,11 +8,11 @@ import boto3
 
 import dask.bag as db
 import dask.dataframe as dd
-from dask.delayed import delayed
 
 import pandas as pd
 
 import requests
+from requests import RequestException, ConnectionError
 from warcio.archiveiterator import ArchiveIterator
 
 from fondant.component import DaskTransformComponent
@@ -18,6 +20,24 @@ from fondant.component import DaskTransformComponent
 logger = logging.getLogger(__name__)
 
 BASE_URL = "https://data.commoncrawl.org/"
+
+
+def get_warc_file_with_retry(warc_file: str) -> str:
+    retries = 0
+    max_retries = 3
+    retry_delay = 5
+
+    while retries < max_retries:
+        try:
+            response = requests.get(BASE_URL + warc_file, stream=True)
+            response.raise_for_status()
+            return response
+        except (RequestException, ConnectionError, ConnectionResetError) as e:
+            logger.error(f"Error downloading WARC file: {e}")
+            logger.error(f"Retrying... {retries}/{max_retries}")
+            time.sleep(retry_delay)
+            retries += 1
+    raise Exception(f"Failed to download WARC file after multiple retries: {warc_file}")
 
 
 def get_records_from_warc_file(warc_file: str, n_records_to_download: int) -> List:
@@ -31,8 +51,7 @@ def get_records_from_warc_file(warc_file: str, n_records_to_download: int) -> Li
     logger.info(f"Processing WARC file from segment path: {warc_file}...")
     records = []
     counter = 0
-    response = requests.get(BASE_URL + warc_file, stream=True)
-    response.raise_for_status()
+    response = get_warc_file_with_retry(warc_file)
 
     for record in ArchiveIterator(response.raw, arc2warc=True):
         if record.rec_type == "response":
@@ -60,16 +79,30 @@ class DownloadCommoncrawlSegments(DaskTransformComponent):
         Returns:
             A Dask DataFrame containing the downloaded webpages.
         """
-        segment_paths = df["segment_path"].to_bag()
+        logger.info(f"len(df): {len(df)}")
+        # df = df.repartition(partition_size="250MB") #if dataframe is too small, we end up with 1 partition which this doesn't solve our OOM problem
+        df = df.repartition(
+            npartitions=2
+        )  # if dataframe is too small, some partitions will be empty
+        logger.info(df)
 
-        records = segment_paths.map(get_records_from_warc_file, n_records_to_download)
-
-        flattened_records = records.flatten()
-        dask_df = flattened_records.to_dataframe(
-            columns=["webpage_url", "webpage_html"]
+        df = df.apply(
+            lambda row: get_records_from_warc_file(
+                row["segment_path"], n_records_to_download
+            ),
+            axis=1,
+            meta=("object"),
         )
+        logger.info(f"len(df): {len(df)}")
+        # df = df.repartition(partition_size="250MB") #looking at the task graph, repartitioning merges the partitions
 
-        return dask_df
+        df = df.explode()
+        df = df.apply(pd.Series, meta={0: "object", 1: "object"})
+        df.columns = ["webpage_url", "webpage_html"]
+
+        df.visualize(filename="download_commoncrawl_segments.png")
+
+        return df
 
 
 if __name__ == "__main__":
