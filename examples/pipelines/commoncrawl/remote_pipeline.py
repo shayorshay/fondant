a6964b8870261868272fcea295f28f1b60c5a27b
Sharon Grundmann <sharon.grundmann@ml6.eu>
2023-07-27 17:58:23 +0200

    Disable partitioning for testing

--- examples/pipelines/commoncrawl/remote_pipeline.py
+++ examples/pipelines/commoncrawl/remote_pipeline.py
@@ -28,6 +28,8 @@ load_from_commoncrawl_op = ComponentOp(
         "n_segments_to_load": 1,
         # "offset": 3
     },
+    node_pool_name="m6a-workers",
+    output_partition_size="disable",
 )
 
 download_commoncrawl_segments_op = ComponentOp(
@@ -36,10 +38,16 @@ download_commoncrawl_segments_op = ComponentOp(
         "n_records_to_download": 100,
         "use_s3": True,
     },
+    node_pool_name="m6a-workers",
+    input_partition_rows="disable",
+    output_partition_size="disable",
 )
 
 extract_image_licenses = ComponentOp(
     component_dir="components/extract_image_licenses",
+    node_pool_name="m6a-workers",
+    input_partition_rows="disable",
+    output_partition_size="disable",
 )
 
 # add ops to pipeline
