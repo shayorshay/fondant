2bd3c778227a15524e817ef9b718f455eeb92985
Sharon Grundmann <sharon.grundmann@ml6.eu>
2023-08-02 11:30:44 +0200

    Add url filter component

--- examples/pipelines/commoncrawl/components/filter_webpage_urls/src/main.py
+++ examples/pipelines/commoncrawl/components/filter_webpage_urls/src/main.py
@@ -1,34 +1,55 @@
 import os
 import logging
-
-import requests
-import tarfile
+from typing import List, Optional
 
 import dask.dataframe as dd
+from urllib.parse import urlparse
 
 from fondant.component import DaskTransformComponent
 from fondant.executor import DaskTransformExecutor
 
-BLACKLIST_URL = (
-    "ftp://ftp.ut-capitole.fr/pub/reseau/cache/squidguard_contrib/blacklists.tar.gz"
-)
+from utils.file_utils import download_tar_file, get_urls_from_file
+
+logger = logging.getLogger(__name__)
+
+BLACKLIST_DIR = "/tmp/blacklists"
+BLACKLIST_URL = "http://dsi.ut-capitole.fr/blacklists/download/blacklists.tar.gz"
 
 
-def download_blacklisted_urls(blacklist_url: str) -> list:
-    response = requests.get(blacklist_url)
-    response.raise_for_status()
+def get_blacklisted_domains(blacklist_dir: str, categories: List[str]) -> List[str]:
+    result = []
+    for category in categories:
+        path = os.path.join(blacklist_dir, "blacklists", category, "domains")
+        urls = get_urls_from_file(path)
+        result.extend(urls)
 
-    temp_file_path = "blacklists.tar.gz"
-    with open(temp_file_path, "wb") as f:
-        f.write(response.content)
-
-    with tarfile.open(temp_file_path, "r:gz") as tar:
-        tar.extractall()
+    return result
 
 
 class FilterWebpageUrls(DaskTransformComponent):
-    def __init__(self, *args):
-        pass
+    def __init__(self, *_, categories: List[str] = None):
+        self.categories = categories
 
     def transform(self, dataframe: dd.DataFrame) -> dd.DataFrame:
-        return None
+        logger.info("Length of dataframe before filtering: %s", len(dataframe))
+
+        download_tar_file(BLACKLIST_URL, BLACKLIST_DIR)
+        blacklisted_domains = get_blacklisted_domains(BLACKLIST_DIR, self.categories)
+        logger.info("Length of blacklisted domains: %s", len(blacklisted_domains))
+
+        dataframe["webpage_base_url"] = dataframe["webpage_url"].apply(
+            lambda x: urlparse(x).netloc, meta=("webpage_url", "object")
+        )
+
+        filtered_df = dataframe[
+            ~dataframe["webpage_base_url"].isin(blacklisted_domains)
+        ]
+        filtered_df = filtered_df.drop(columns=["webpage_base_url"])
+
+        logger.info("Length of dataframe after filtering: %s", len(filtered_df))
+        return filtered_df
+
+
+if __name__ == "__main__":
+    executor = DaskTransformExecutor.from_args()
+    executor.execute(FilterWebpageUrls)
