cfbcc2c2c12c2f941213343f3ff759be03b4b5f2
Sharon Grundmann <sharon.grundmann@ml6.eu>
2023-09-19 10:08:30 +0200

    Pipeline for laion testing

---
+++ examples/pipelines/commoncrawl/components/extract_image_licenses/src/main.py
@@ -0,0 +1,110 @@
+import re
+import logging
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from typing import List, Optional
+
+from fondant.component import PandasTransformComponent
+from fondant.executor import PandasTransformExecutor
+
+from utils.license_utils import get_license_type, get_license_location
+from utils.image_utils import get_images_from_soup, get_unique_images
+
+logger = logging.getLogger(__name__)
+
+
+def get_image_info_from_webpage(webpage_url: str, webpage_html: str) -> List[str]:
+    """Extracts image urls and license metadata from the parsed html code.
+    Args:
+        webpage_url: The url of the webpage.
+        webpage_html: The html content of the webpage.
+    Returns:
+        A list of image urls and license metadata.
+    """
+    license_type = None
+    license_location = None
+
+    try:
+        soup = BeautifulSoup(webpage_html, "html.parser")
+        for a_tag in soup.find_all("a"):
+            if a_tag.has_attr("href"):
+                license_type = get_license_type(a_tag)
+                if license_type is not None:
+                    license_location = get_license_location(a_tag)
+
+                    if license_location is None:
+                        continue
+                    else:
+                        logger.info(
+                            f"Found license type: {license_type} at {license_location}"
+                        )
+                        break
+
+                    # images = get_images_from_soup(
+                    #     soup, webpage_url, license_type, license_location
+                    # )
+                    # unique_images = get_unique_images(images)
+
+                    # return unique_images
+
+        if license_type is not None and license_location is not None:
+            images = get_images_from_soup(
+                soup, webpage_url, license_type, license_location
+            )
+            unique_images = get_unique_images(images)
+
+            return unique_images
+
+    except Exception as e:
+        logger.error(f"Error parsing HTML: {e}")
+        return None
+
+
+class ExtractImageLicenses(PandasTransformComponent):
+    def __init__(self, *_, deduplicate: Optional[bool] = True):
+        self.deduplicate = deduplicate
+
+    def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """Extracts image url and license from the HTML content.
+        Args:
+            dataframe: A pandas dataframe with the webpage url and html content.
+        Returns:
+            A pandas dataframe with the image url and license metadata.
+        """
+        logger.info(f"Extracting images from {len(dataframe)} webpages")
+
+        images = dataframe.apply(
+            lambda row: get_image_info_from_webpage(
+                row[("webpage", "url")], row[("webpage", "content")]
+            ),
+            axis=1,
+        )
+        images = images.dropna()
+        images = [item for sublist in images for item in sublist]
+
+        images_df = pd.DataFrame(
+            images,
+            columns=[
+                ("image", "image_url"),
+                ("image", "alt_text"),
+                ("image", "webpage_url"),
+                ("image", "license_type"),
+                ("image", "license_location"),
+            ],
+        )
+
+        images_df.index = images_df.index.astype(str)
+
+        if not images_df.empty and self.deduplicate:
+            images_df = images_df.drop_duplicates(
+                subset=[("image", "image_url")], keep="first"
+            )
+
+        logger.info(f"Extracted {len(images_df)} images")
+        return images_df
+
+
+if __name__ == "__main__":
+    executor = PandasTransformExecutor.from_args()
+    executor.execute(ExtractImageLicenses)
---
+++ examples/pipelines/commoncrawl/laion_pipeline.py
@@ -0,0 +1,45 @@
+import logging
+import sys
+
+sys.path.append("../")
+
+from aws_pipeline_configs import PipelineConfigs
+
+from fondant.pipeline import ComponentOp, Pipeline
+
+logger = logging.getLogger(__name__)
+# General configs
+pipeline_name = "commoncrawl-pipeline"
+pipeline_description = "Pipeline that downloads from commoncrawl"
+
+
+load_from_commoncrawl_op = ComponentOp(
+    component_dir="components/load_from_commoncrawl",
+    arguments={
+        "index_name": "CC-MAIN-2023-23",
+        "n_segments_to_load": 1,
+    },
+    cache=False,
+)
+
+download_from_commonwarc_op = ComponentOp(
+    component_dir="components/download_commoncrawl_segments",
+    arguments={
+        "n_records_to_download": 100,
+        "use_s3": "False",
+    },
+    cache=False,
+)
+
+extract_images_from_warc_op = ComponentOp(
+    component_dir="components/extract_image_licenses",
+    cache=False,
+)
+
+
+pipeline = Pipeline(pipeline_name=pipeline_name, base_path=PipelineConfigs.BASE_PATH)
+
+
+pipeline.add_op(load_from_commoncrawl_op)
+pipeline.add_op(download_from_commonwarc_op, dependencies=[load_from_commoncrawl_op])
+pipeline.add_op(extract_images_from_warc_op, dependencies=[download_from_commonwarc_op])
